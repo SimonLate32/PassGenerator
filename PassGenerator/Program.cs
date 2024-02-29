147178621845758e77a94bff9114c8acc28b1598
SimonLate32 <eles786@gmail.com>
2024-02-29 18:50:30 +0300

    123

--- PassGenerator/Program.cs
+++ PassGenerator/Program.cs
@@ -2,39 +2,26 @@
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        Console.Write("Введите идентификатор пользователя:");
-        string userId = Console.ReadLine();
-
-        string password = GenPass(userId);
+        Console.Write("Введите ваше имя пользователя:");
+        string userID = Console.ReadLine(); //Записываем имя пользователя в переменную userID
+        string password = GeneratePassword(userID); // Передаем фунции значение с userID
         Console.Write("Сгенерированный пароль: " + password);
     }
 
-    static string GenPass(string userId)
+    static string GeneratePassword(string userID)
     {
-        Random rand = new Random();
-
-        // Получаем b1 и b2 из идентификатора пользователя
-        char b1 = userId[rand.Next(userId.Length)];
-        char b2 = userId[rand.Next(userId.Length)];
-
-        // Получаем N^2 (в качестве примера возьмем случайное число от 1 до 100)
-        int N = rand.Next(1, 101);
-        int N2 = N * N;
-
-        // Вычисляем b3 по заданной формуле
-        int b3 = N2 % 10;
-
-        // Генерируем остальные символы
-        int b4 = rand.Next(0, 10);
-        char[] SpecChar = { '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*' };
-        char b5 = SpecChar[rand.Next(SpecChar.Length)];
-        char b6 = (char)rand.Next(97, 123); // случайная малая буква английского алфавита
-
-        // Собираем пароль из полученных символов
-        string password = string.Concat(b1, b2, b3, b4, b5, b6);
-
-        return password;
+        Random randomPass = new Random();
+        char b1 = (char)('a' + randomPass.Next(0, 26));//случайные строчные буква
+        char b2 = (char)('a' + randomPass.Next(0, 26));
+        char b3 = userID[randomPass.Next(userID.Length)];//случайная буква из userID
+        char b4 = (char)('A' + randomPass.Next(0, 26));//случайная заглавная буква
+        char b5 = (char)('A' + randomPass.Next(0, 26));
+        int N = randomPass.Next(1, 101); // выбор случайного числа для N от 1 до 100 
+        int result = (int)(Math.Pow(N, 4) % 100);
+        string b6 = (result < 10) ? "0" : (result / 10).ToString();//символ для пароля на основе результата вычислений
+        string b7 = (result < 10) ? result.ToString() : (result % 10).ToString();
+        return $"{b1}{b2}{b3}{b4}{b5}{b6}{b7}"; //объеденение и возвращение всех символов
     }
 }
